a4479b62c9408d220ed3fd3318b9ee6844ff0ca9
martindelosrios <martindelosrios13@gmail.com>
2023-09-19 14:07:22 +0200

    fix with black and flake8

--- pyROGER/models.py
+++ pyROGER/models.py
@@ -1,23 +1,22 @@
-from pyROGER.roger import roger_model
+from pyROGER.roger import RogerModel
 import pandas as pd
-import os
 import numpy as np
 import pkg_resources
 
-DATA_PATH = pkg_resources.resource_filename('pyROGER', '../dataset/')
 
-data = pd.read_csv(DATA_PATH + '/highMass_trainset_roger1.csv', sep = ' ')
+DATA_PATH = pkg_resources.resource_filename("pyROGER", "../dataset/")
+
+data = pd.read_csv(DATA_PATH + "/highMass_trainset_roger1.csv", sep=" ")
 data = np.asarray(data)
-data[np.where(data[:,2] == 'CL')[0], 2] = 0
-data[np.where(data[:,2] == 'RIN')[0], 2] = 1
-data[np.where(data[:,2] == 'BS')[0], 2] = 2
-data[np.where(data[:,2] == 'IN')[0], 2] = 3
-data[np.where(data[:,2] == 'ITL')[0], 2] = 4
+data[np.where(data[:, 2] == "CL")[0], 2] = 0
+data[np.where(data[:, 2] == "RIN")[0], 2] = 1
+data[np.where(data[:, 2] == "BS")[0], 2] = 2
+data[np.where(data[:, 2] == "IN")[0], 2] = 3
+data[np.where(data[:, 2] == "ITL")[0], 2] = 4
 
-data = data.astype('float64')
+data = data.astype("float64")
 
 comments = """ ROGER model for isolated galaxy clusters with masses bigger than >10^{13} M_{sun}.
   This model is equivalent to the one presented in XXXX.XXXX and in ROGERWebsite.
 """
-highMass_roger1 = roger_model(data[:,(0,1)], data[:,2], comments = comments)
-#highMass_roger1.train()
+HighMassRoger1 = RogerModel(data[:, (0, 1)], data[:, 2], comments=comments)
--- pyROGER/roger.py
+++ pyROGER/roger.py
@@ -1,17 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import sklearn as sk
-#import pandas as pd
-
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm
 
-from mlxtend.plotting import plot_confusion_matrix
 
-class roger_model():
+class RogerModel:
     """
     Main class of pyROGER. This object contains all the information
     of the classification.
@@ -19,18 +13,19 @@ class roger_model():
     Attributes
     ----------
     x_dataset : np.array
-        Numpy array containing the features used for the classification. 
+        Numpy array containing the features used for the classification.
         Usually these will be the relative velocity normalized to the velocity
-        dispersion and the normalized radii.    
+        dispersion and the normalized radii.
     y_dataset : np.array
-        Numpy array containing the real classes of the galaxies. Usually these are
-        cluster, recent infalling, backsplash, infalling and interloper galaxies.
+        Numpy array containing the real classes of the galaxies. Usually these
+        are cluster, recent infalling, backsplash, infalling and interloper
+        galaxies.
     ml_models : list
         List with the machine learning methods that will be used. The default
-        methods are KNN, random forest and SVM. 
+        methods are KNN, random forest and SVM.
     train_percentage : float
-        Float between 0 and 1 representing the percentage of observations that will
-        be used for training.
+        Float between 0 and 1 representing the percentage of observations that
+        will be used for training.
     comments: str
         String adding information for the trained model.
 
@@ -39,38 +34,48 @@ class roger_model():
     splt()
         Split the data into training and testing set. It returns the indices
         of both sets.
-        
+
     train()
         Train all the methods contained in ml_models.
-        
+
     predict(data, n_model)
         Predict the class of the observation contained in data using the model
         number n_model.
-        
+
     """
-    def __init__(self, x_dataset:np.array, y_dataset:np.array, 
-                 ml_models = [KNeighborsClassifier(n_neighbors=3),
-                              RandomForestClassifier(max_depth=2, random_state=0),
-                              svm.SVC(probability = True)], 
-                 train_percentage = 0.75,
-                labels = ['CL', 'RIN', 'BS', 'IN', 'ITL'],
-                comments = None):
+
+    def __init__(
+        self,
+        x_dataset: np.array,
+        y_dataset: np.array,
+        ml_models=[
+            KNeighborsClassifier(n_neighbors=3),
+            RandomForestClassifier(max_depth=2, random_state=0),
+            svm.SVC(probability=True),
+        ],
+        train_percentage=0.75,
+        labels=["CL", "RIN", "BS", "IN", "ITL"],
+        comments=None,
+    ):
         self.x_dataset = x_dataset
         self.y_dataset = y_dataset
         self.n_obs = len(x_dataset)
         self.ml_models = ml_models
         self.num_models = len(ml_models)
         self.train_percentage = train_percentage
-        self.train_indices, self.test_indices = self.split() 
+        self.train_indices, self.test_indices = self.split()
         self.labels = labels
         self.trained = False
-        if comments is not None: self.comments = comments
+        if comments is not None:
+            self.comments = comments
 
     def __repr__(self):
-        if self.trained == False: self.comments = self.comments + '\n NOT TRAINED YET'
-        if self.trained == True: self.comments = self.comments + '\n Ready to use'
+        if self.trained is False:
+            self.comments = self.comments + "\n NOT TRAINED YET"
+        if self.trained is True:
+            self.comments = self.comments + "\n Ready to use"
         return self.comments
-        
+
     def split(self):
         """
         Function for splitting the dataset into train and test set.
@@ -80,23 +85,26 @@ class roger_model():
 
         Tuple with the indices of the training and testing sets.
         """
-        ran_indices = np.random.choice(np.arange(self.n_obs), size = self.n_obs)
-        train_indices = ran_indices[:round(self.train_percentage * self.n_obs)]
-        test_indices = ran_indices[round(self.train_percentage * self.n_obs):]
-        
+        ran_indices = np.random.choice(np.arange(self.n_obs), size=self.n_obs)
+        train_indices = ran_indices[: round(self.train_percentage * self.n_obs)]
+        test_indices = ran_indices[round(self.train_percentage * self.n_obs) :]
+
         return train_indices, test_indices
-        
+
     def train(self):
         """
-        Function for training the machine learning methods.       
+        Function for training the machine learning methods.
         """
-        
+
         for i in self.ml_models:
-            i.fit(self.x_dataset[self.train_indices,:], self.y_dataset[self.train_indices])
+            i.fit(
+                self.x_dataset[self.train_indices, :],
+                self.y_dataset[self.train_indices],
+            )
         self.trained = True
         return None
-        
-    def predict_class(self, data:np.array, n_model:int) -> np.array:
+
+    def predict_class(self, data: np.array, n_model: int) -> np.array:
         """
         Function for predicting the orbital class.
 
@@ -106,7 +114,7 @@ class roger_model():
         data:np.array
             Numpy array containing the features that will be used for
             the classification.
-            
+
         n_model:int
             Number of the machine learning methods that will be used
             for the classification.
@@ -118,8 +126,8 @@ class roger_model():
         """
         orbital_class = self.ml_models[n_model].predict(data)
         return orbital_class
-        
-    def predict_prob(self, data:np.array, n_model:int) -> np.array:
+
+    def predict_prob(self, data: np.array, n_model: int) -> np.array:
         """
         Function for predicting the orbital class probability.
 
@@ -129,7 +137,7 @@ class roger_model():
         data:np.array
             Numpy array containing the features that will be used for
             the classification.
-            
+
         n_model:int
             Number of the machine learning methods that will be used
             for the classification.
@@ -142,7 +150,7 @@ class roger_model():
         orbital_prob = self.ml_models[n_model].predict_proba(data)
         return orbital_prob
 
-    def confusion_matrix(self, real_class = None, pred_class = None, n_model:int = 0):
+    def confusion_matrix(self, real_class=None, pred_class=None, n_model: int = 0):
         """
         Function for computing the confusion matrix.
 
@@ -151,20 +159,20 @@ class roger_model():
         n_model:int
             Number of the machine learning methods that will be used
             for the classification.
-            
+
         real_class:np.array
             Numpy array containing the real classes. If nothing is pass, the
             function will use the testset classes.
-            
+
         pred_class:np.array
-            Numpy array containing the predicted classes. If nothing is pass, the
-            function will use the classes predicted for the testset.
+            Numpy array containing the predicted classes. If nothing is pass,
+            the function will use the classes predicted for the testset.
         """
-        if real_class is None: 
+        if real_class is None:
             real_class = self.y_dataset[self.test_indices]
-        if pred_class is None: 
-            pred_class = self.predict_class(data = self.x_dataset[self.test_indices,:], n_model = n_model)
+        if pred_class is None:
+            pred_class = self.predict_class(
+                data=self.x_dataset[self.test_indices, :], n_model=n_model
+            )
         conf_mat = sk.metrics.confusion_matrix(real_class, pred_class)
         return conf_mat
-
-
