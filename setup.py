ea3e825ad1a8eec86a605819229a406b4c75ca6a
martindelosrios <martindelosrios13@gmail.com>
2023-09-19 20:40:20 +0200

    flake

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open ("README.md", "r") as fp:
+with open("README.md", "r") as fp:
     LONG_DESCRIPTION = fp.read()
 
 REQUIREMENTS = ["numpy", "matplotlib", "scikit-learn", "mlxtend", "pandas"]
 
 setup(
-    name = "pyROGER",
-    version = "0.1",
-    description = "Implementation of phase function for asteroids in Python",
-    long_description = LONG_DESCRIPTION,
-    long_description_content_type = "text/markdown",
-    author = "Martin de los Rios",
-    author_email = " martindelosrios13@gmail.com ",
-    url = " https://github.com/martindelosrios/pyROGER",    
-    py_modules = ["ez_setup" ], # < - - - - - - - aca van los modulos
-    packages = ["pyROGER", "dataset"], # < - -- - - - - aca van los paquetes
-    #packages = find_packages(exclude=['codes_*']),
-    #include_package_data = True, # < - - - - - -- solo si hay datos
-    license = "The MIT License",
-    install_requires  = REQUIREMENTS,
-    keywords = ["pyROGER", "backsplash", "galaxies"],
-    classifiers = [
+    name="pyROGER",
+    version="0.1",
+    description="Implementation of phase function for asteroids in Python",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Martin de los Rios",
+    author_email=" martindelosrios13@gmail.com ",
+    url=" https://github.com/martindelosrios/pyROGER",
+    py_modules=["ez_setup"],  # < - - - - - - - aca van los modulos
+    packages=["pyROGER", "dataset"],  # < - -- - - - - aca van los paquetes
+    # packages = find_packages(exclude=['codes_*']),
+    # include_package_data = True, # < - - - - - -- solo si hay datos
+    license="The MIT License",
+    install_requires=REQUIREMENTS,
+    keywords=["pyROGER", "backsplash", "galaxies"],
+    classifiers=[
         " Development Status :: 4 - Beta",
         " Intended Audience :: Education",
         " Intended Audience :: Science/Research",
@@ -31,5 +31,5 @@ setup(
         " Programming Language :: Python :: 3.8",
         " Programming Language :: Python :: Implementation :: CPython",
         " Topic :: Scientific/Engineering",
-    ]
+    ],
 )
