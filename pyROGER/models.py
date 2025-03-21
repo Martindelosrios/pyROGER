8c213703a31a4f2d9c42eeca101a95178349b95e
Martin de los Rios <martindelosrios13@gmail.com>
2025-03-21 11:59:37 +0100

    I saved the models

--- pyROGER/models.py
+++ pyROGER/models.py
@@ -22,3 +22,7 @@ bigger than >10^{13} M_{sun}.
   in https://mdelosrios.shinyapps.io/roger_shiny/.
 """
 HighMassRoger1 = RogerModel(data[:, (0, 1)], data[:, 2], comments=comments)
+
+HighMassRoger1.train(path_to_saved_models = [DATA_PATH + '/HighMassRoger1_KNN.joblib',
+                                             DATA_PATH + '/HighMassRoger1_RF.joblib',
+                                             DATA_PATH + '/HighMassRoger1_SVM.joblib'])
