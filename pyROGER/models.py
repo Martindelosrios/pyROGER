5b3ff8ee85d01586f9e1791ad1cb1ae0ee501450
Martin de los Rios <martindelosrios13@gmail.com>
2025-09-24 10:31:44 -0300

    just add more comments

--- pyROGER/models.py
+++ pyROGER/models.py
@@ -40,12 +40,20 @@ bigger than >10^{13} M_{sun}.
     [:,0] = R / R200
     [:,1] = |\Delta V| / sigma
 
-  The output is a np.array with shape (Ngal, 5)
-  [:,0] = P_cl: Probability of being a cluster galaxy.
-  [:,1] = P_rin: Probability of being a backsplash galaxy.
-  [:,2] = P_bs: Probability of being a recent infaller galaxy.
-  [:,3] = P_in: Probability of being an infalling galaxy.
-  [:,4] = P_itl: Probability of being a iterloper galaxy.
+  The output for predict_prob() is a np.array with shape (Ngal, 5)
+    [:,0] = P_cl: Probability of being a cluster galaxy.
+    [:,1] = P_rin: Probability of being a backsplash galaxy.
+    [:,2] = P_bs: Probability of being a recent infaller galaxy.
+    [:,3] = P_in: Probability of being an infalling galaxy.
+    [:,4] = P_itl: Probability of being a iterloper galaxy.
+  
+  The output for predict_class() is a np.array with shape (Ngal, ) with
+  the predicted classes. 
+     0: Cluster galaxy 
+     1: Recent infaller galaxy 
+     2: Backsplash galaxy 
+     3: Infalling galaxy 
+     4: Iterlooper galaxy 
 """
 HighMassRoger1 = RogerModel(data[:, (0, 1)], data[:, 2], comments=comments)
 
@@ -65,11 +73,19 @@ bigger than >10^{13} M_{sun}.
     [:,1] = R / R200
     [:,2] = |\Delta V| / sigma
 
-  The output is a np.array with shape (Ngal, 5)
-  [:,0] = P_cl: Probability of being a cluster galaxy.
-  [:,1] = P_bs: Probability of being a backsplash galaxy.
-  [:,2] = P_rin: Probability of being a recent infaller galaxy.
-  [:,3] = P_in: Probability of being an infalling galaxy.
-  [:,4] = P_itl: Probability of being a iterloper galaxy.
+  The output predict_prob() is a np.array with shape (Ngal, 5)
+    [:,0] = P_cl: Probability of being a cluster galaxy.
+    [:,1] = P_bs: Probability of being a backsplash galaxy.
+    [:,2] = P_rin: Probability of being a recent infaller galaxy.
+    [:,3] = P_in: Probability of being an infalling galaxy.
+    [:,4] = P_itl: Probability of being a iterloper galaxy.
+  
+  The output for predict_class() is a np.array with shape (Ngal, ) with
+  the predicted classes. 
+     0: Cluster galaxy 
+     1: Backsplash galaxy 
+     2: Recent infaller galaxy 
+     3: Infalling galaxy 
+     4: Iterlooper galaxy 
 """
 Roger2 = RogerModel(data[:, (0, 1)], data[:, 2], comments=comments,  ml_models = [KNeighborsClassifier(n_neighbors=63)])
