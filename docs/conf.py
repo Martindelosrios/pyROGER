4fcbb2c61c8dd2238676ce0b731e418b192a0ffe
Martin de los Rios <martindelosrios13@gmail.com>
2025-10-02 10:09:17 -0300

    minor change to docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,8 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath("..", "pyROGER"))
+#sys.path.insert(0, os.path.abspath(".."))
 
+sys.path.insert(0, os.path.abspath(os.path.join("..", "pyROGER")))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
