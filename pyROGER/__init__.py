43941fa8435eec4704f9bd806726b0c3caf4211b
Martin de los Rios <martindelosrios13@gmail.com>
2026-05-08 10:27:36 -0300

    minor change

--- pyROGER/__init__.py
+++ pyROGER/__init__.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 today = date.today()
-if (today.day == 8) & (today.month == 6):
+if (today.day == 8) & (today.month == 5):
     print('Today is the {} th aniversary of the epic Libertadores final won by River Plate against boca, for those who celebrate that day, congratulations (que distintos que somos)!!'.format(today.year - 2018) + 
     '''
 ⠀⠀⠀⣠⡾⠛⠶⠦⣤⣄⣀⣀⣀⣀⣀⣀⣀⣠⣤⠴⠶⡛⢷⣄⠀⠀⠀
